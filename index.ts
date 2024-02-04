461751f4b565c8892dd11cbda2f713d4e6059cb7
DanMeyer <mindwalkman@gmail.com>
2024-02-04 18:16:35 +0000

    Validate that we can pass assertion errors through to the user

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
 
 import { board } from "./src/board";
+import { Point } from "./src/point";
 
 console.log("board: ", board);
 
@@ -15,6 +16,18 @@ app.get("/", (req: Request, res: Response) => {
 
 app.get("/board", (req: Request, res: Response) => {
   res.send(board);
+});
+
+app.get("/checkValidPoint", (req: Request, res: Response) => {
+  const pointLabel: string = (typeof req.query.point === "string") ? req.query.point : "";
+  console.log("pointLabel", pointLabel);
+  const point = new Point(pointLabel);
+  res.send(point);
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  res.status(400).send({ message: err.message });
 });
 
 app.listen(port, () => {
