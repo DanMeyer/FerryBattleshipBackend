f8dc45b01b652b7028aa26f3c5a31e7069c323af
DanMeyer <mindwalkman@gmail.com>
2024-02-07 03:07:36 +0000

    tiny game tests

--- tests/game.test.ts
+++ tests/game.test.ts
@@ -1,15 +1,30 @@
 import { describe, expect, test } from '@jest/globals';
-import exp from 'constants';
-import { Board } from '../src/board';
-import { Piece } from '../src/piece';
 import { Game } from '../src/game';
 
 describe("Game", () => {
-  test("Player can attack no pieces placed", () => {
+  test("Player can not attack if no pieces have placed", () => {
     const game = new Game();
+    game.placePiece("G1", "G4");
+
+    expect(() => {
+      game.attackAndReply("F4");
+    }).toThrow();
   });
   
-  test("Player can not attack if only 3 pieces placed", () => {
+  test("Player can attack if 4 pieces placed", () => {
+    const game = new Game();
+    game.placePiece("G1", "G3");
+    game.placePiece("H2", "H4");
+    game.placePiece("I2", "I5");
+    game.placePiece("A1", "A5");
+
+    expect(() => {
+      game.attackAndReply("F4");
+    }).not.toThrow();
   });
+
+  /*
+    TODO: figure out a testing strategy for deeper into the game
+  */
 
 });
